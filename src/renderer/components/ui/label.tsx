f93afc2e1f5a34aca18b3d50b246f6fd2188da11
Chihab Derghal <chihabderghal1@gmail.com>
2025-07-11 16:53:32 +0100

    Fix: Change Shdcn UI paths

--- src/renderer/components/ui/label.tsx
+++ src/renderer/components/ui/label.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 
-import { cn } from "@/renderer/lib/utils"
+import { cn } from "@renderer/lib/utils"
 
 function Label({
   className,
